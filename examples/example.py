9003d18b617bc4450101865a32d705972857e599
Adam Neumann <adam@noizwaves.com>
2012-03-10 20:32:42 +1100

    Tidied up example dragonfile

--- examples/example.py
+++ examples/example.py
@@ -2,16 +2,8 @@
 
 from dragons.util import collect_cookies
 
-__version__ = '1'
-__all__ = ['SETTINGS', 'start']
-
-SETTINGS = {
-    'host': 'dev1.indexmedia.com',
-    'protocol': 'http',
-    'concurrency': 1,
-}
-
 def start(client, context):
-    response = client.get('/tag/media/?media_url=http://www.google.com/icon.png')
+    response = client.get('/api/')
     collect_cookies(response, client)
-    return start
+
+    return start # recurse
